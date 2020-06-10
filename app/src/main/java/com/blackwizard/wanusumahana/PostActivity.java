17ff9d06559b6cd9a5cb603881ed77a59d724e5a
Your Name <minozoyjustin@gmail.com>
2020-06-10 17:18:32 -0400

    Coding PostActivity, saving post information to database with unique id, Profile image and the post that include image and description of the image

--- app/src/main/java/com/blackwizard/wanusumahana/PostActivity.java
+++ app/src/main/java/com/blackwizard/wanusumahana/PostActivity.java
@@ -5,6 +5,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -18,12 +19,19 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.HashMap;
 
 
 public class PostActivity extends AppCompatActivity {
@@ -41,17 +49,35 @@ public class PostActivity extends AppCompatActivity {
 
     private String saveCurrentDate, saveCurrentTime, postRandomName;
 
+    private String downloadUrl;
+
+    private DatabaseReference usersReference, postsReference;
+
+    private FirebaseAuth firebaseAuth;
+
+    private String currentUsreID;
+
+    private ProgressDialog loadingBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+        currentUsreID = firebaseAuth.getCurrentUser().getUid();
+
         postImagesReference = FirebaseStorage.getInstance().getReference();
+        usersReference = FirebaseDatabase.getInstance().getReference().child("Users");
+        postsReference = FirebaseDatabase.getInstance().getReference().child("Posts");
+
 
         selectPostImage = findViewById(R.id.updatePostImageButton);
         postDescription = findViewById(R.id.postDescription);
         updatePostButton = findViewById(R.id.updatePostButton);
+
+        loadingBar = new ProgressDialog(this);
 
         mToolbar = findViewById(R.id.updatePostToolBar);
         setSupportActionBar(mToolbar);
@@ -88,6 +114,11 @@ public class PostActivity extends AppCompatActivity {
         }
         else
         {
+            loadingBar.setTitle("Add New Post");
+            loadingBar.setMessage("Please wait, while we are updating your new post!");
+            loadingBar.show();
+            loadingBar.setCanceledOnTouchOutside(true);
+
             storingImageToFirebaseStorage();
         }
     }
@@ -100,7 +131,7 @@ public class PostActivity extends AppCompatActivity {
 
         Calendar callForTime = Calendar.getInstance();
         SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm");
-        saveCurrentDate = currentTime.format(callForDate.getTime());
+        saveCurrentDate = currentTime.format(callForTime.getTime());
 
         postRandomName = saveCurrentDate + saveCurrentTime;
 
@@ -110,12 +141,65 @@ public class PostActivity extends AppCompatActivity {
             public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
                 if (task.isSuccessful())
                 {
+
+                    // downloadUrl = task.getResult().getDownloadUrl().toString(); the line has an error
+                    downloadUrl = task.getResult().getStorage().getDownloadUrl().toString(); //corrected with this
+
                     Toast.makeText(PostActivity.this, "Image Uploaded successfully to Firebase", Toast.LENGTH_SHORT).show();
+                    
+                    savingPostInformationToDatabase();
                 }
                 else {
                     String message = task.getException().getMessage();
                     Toast.makeText(PostActivity.this, "Error Occured." + message , Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+    }
+
+    private void savingPostInformationToDatabase() {
+        usersReference.child(currentUsreID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists())
+                {
+                    String userFullName = dataSnapshot.child("fullname").getValue().toString();
+                    String userProfileImage = dataSnapshot.child("Profile Image").getValue().toString();
+
+                    HashMap postMap = new HashMap();
+                    postMap.put("uid", currentUsreID);
+                    postMap.put("date", saveCurrentDate);
+                    postMap.put("time", saveCurrentTime);
+                    postMap.put("description", description);
+                    postMap.put("postImage", downloadUrl);
+                    postMap.put("profileImage", userProfileImage);
+                    postMap.put("fullname", userFullName );
+
+                    postsReference.child(currentUsreID + postRandomName).updateChildren(postMap).addOnCompleteListener(new OnCompleteListener() {
+                        @Override
+                        public void onComplete(@NonNull Task task) {
+                            if(task.isSuccessful())
+                            {
+                                sendUserToMainActivity();
+
+                                Toast.makeText(PostActivity.this, "New Post is updated Successfully", Toast.LENGTH_SHORT).show();
+
+                                loadingBar.dismiss();
+                            }
+                            else {
+                                Toast.makeText(PostActivity.this, "Error Occured while updating your Post.", Toast.LENGTH_SHORT).show();
+
+                                loadingBar.dismiss();
+                            }
+                        }
+                    });
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
             }
         });
     }
