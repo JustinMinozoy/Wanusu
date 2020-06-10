a753de7c54393316296a0ba273b454b279e4b7b6
Your Name <minozoyjustin@gmail.com>
2020-06-10 11:33:01 -0400

    coding PostActivity to save post image and description to database storage

--- app/src/main/java/com/blackwizard/wanusumahana/PostActivity.java
+++ app/src/main/java/com/blackwizard/wanusumahana/PostActivity.java
@@ -1,16 +1,29 @@
 package com.blackwizard.wanusumahana;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 
 public class PostActivity extends AppCompatActivity {
@@ -21,11 +34,20 @@ public class PostActivity extends AppCompatActivity {
     private Button updatePostButton;
 
     private static final int galleryPick = 1;
+    private Uri imageUri;
+
+    private String description;
+    private StorageReference postImagesReference;
+
+    private String saveCurrentDate, saveCurrentTime, postRandomName;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        postImagesReference = FirebaseStorage.getInstance().getReference();
 
         selectPostImage = findViewById(R.id.updatePostImageButton);
         postDescription = findViewById(R.id.postDescription);
@@ -45,6 +67,57 @@ public class PostActivity extends AppCompatActivity {
             }
         });
 
+        updatePostButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validatePostInfo();
+            }
+        });
+
+    }
+
+    private void validatePostInfo() {
+        description = postDescription.getText().toString();
+        if (imageUri == null) {
+            Toast.makeText(this, "Please select post image", Toast.LENGTH_SHORT).show();
+
+        }
+        else if (TextUtils.isEmpty(description))
+        {
+            Toast.makeText(this, "Please describe your post", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            storingImageToFirebaseStorage();
+        }
+    }
+
+    private void storingImageToFirebaseStorage() {
+
+        Calendar callForDate = Calendar.getInstance();
+        SimpleDateFormat currentDate = new SimpleDateFormat("dd-MMMM-yyyy");
+        saveCurrentDate = currentDate.format(callForDate.getTime());
+
+        Calendar callForTime = Calendar.getInstance();
+        SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm");
+        saveCurrentDate = currentTime.format(callForDate.getTime());
+
+        postRandomName = saveCurrentDate + saveCurrentTime;
+
+        StorageReference filePath  = postImagesReference.child("Post Images").child(imageUri.getLastPathSegment() + postRandomName +".jpg");
+        filePath.putFile(imageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                if (task.isSuccessful())
+                {
+                    Toast.makeText(PostActivity.this, "Image Uploaded successfully to Firebase", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    String message = task.getException().getMessage();
+                    Toast.makeText(PostActivity.this, "Error Occured." + message , Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     private void openGallery() {
@@ -53,6 +126,16 @@ public class PostActivity extends AppCompatActivity {
         galleryIntent.setType("image/*");
         startActivityForResult(galleryIntent, galleryPick);
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == galleryPick && resultCode == RESULT_OK && data != null)
+        {
+            imageUri = data.getData();
+            selectPostImage.setImageURI(imageUri);
+        }
     }
 
     @Override
