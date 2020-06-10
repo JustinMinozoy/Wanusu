925c2c0c061f6b195e2d3162e317f09d6eba5571
Your Name <minozoyjustin@gmail.com>
2020-06-10 09:39:50 -0400

    fixing the bug, designing PostActivity xml for a user to select a post image with its description

--- app/src/main/java/com/blackwizard/wanusumahana/PostActivity.java
+++ app/src/main/java/com/blackwizard/wanusumahana/PostActivity.java
@@ -7,16 +7,29 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 
 public class PostActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
+    private ImageButton selectPostImage;
+    private EditText postDescription;
+    private Button updatePostButton;
+
+    private static final int galleryPick = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        selectPostImage = findViewById(R.id.updatePostImageButton);
+        postDescription = findViewById(R.id.postDescription);
+        updatePostButton = findViewById(R.id.updatePostButton);
 
         mToolbar = findViewById(R.id.updatePostToolBar);
         setSupportActionBar(mToolbar);
@@ -25,13 +38,28 @@ public class PostActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setTitle("Update Post");
 
+        selectPostImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
+
+    }
+
+    private void openGallery() {
+        Intent galleryIntent = new Intent();
+        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, galleryPick);
+
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
         int id = item.getItemId();
-        if (id == R.id.nav_home)
+        if (id == android.R.id.home)
             {
                 sendUserToMainActivity();
         }
