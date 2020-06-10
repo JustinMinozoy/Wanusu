4244bee890e2abb88f161d104d179ba4fa24e15f
Your Name <minozoyjustin@gmail.com>
2020-06-09 20:26:44 -0400

    adding appBar layoout to PostActivity xml and toolbar back button

--- app/src/main/java/com/blackwizard/wanusumahana/PostActivity.java
+++ app/src/main/java/com/blackwizard/wanusumahana/PostActivity.java
@@ -1,14 +1,47 @@
 package com.blackwizard.wanusumahana;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
 
 public class PostActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        mToolbar = findViewById(R.id.updatePostToolBar);
+        setSupportActionBar(mToolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("Update Post");
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == R.id.nav_home)
+            {
+                sendUserToMainActivity();
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    private void sendUserToMainActivity() {
+
+        Intent mainIntent = new Intent(PostActivity.this, MainActivity.class);
+        startActivity(mainIntent);
     }
 }
